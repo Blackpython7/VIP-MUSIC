3b792542921357a25016ff896fc3a8829fd9f6ef
AVYA <136340837+Blackpython7@users.noreply.github.com>
2024-06-11 12:01:58 +0530

    Update zowner.py

--- VIPMUSIC/plugins/USERBOT/zowner.py
+++ VIPMUSIC/plugins/USERBOT/zowner.py
@@ -11,12 +11,12 @@ from VIPMUSIC.utils.database import get_assistant
 async def repo(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/4b52da6d880cbb199298a.jpg",
-        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱[𝐑𝐄𝐏𝐎](https://github.com/THE-VIP-BOY-OP/VIP-MUSIC)🍁""",
+        caption=f"""🍁𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐆𝐄𝐓🌱[𝐑𝐄𝐏𝐎](https://t.me/userrepo)🍁""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🌱ƨσʋяcɛ🌱", url=f"https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
+                        "🌱ƨσʋяcɛ🌱", url=f"https://t.me/userrepo"
                     )
                 ]
             ]
